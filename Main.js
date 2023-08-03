4861ee4e1e8d6c4e05339320ce97d481c4835535
shortpupper <78228290+shortpupper@users.noreply.github.com>
2023-08-03 18:45:56 -0500

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, ChatGPT User!</Text>
+      <Text style={styles.subtitle}>Welcome to the ChatGPT Android App!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#555',
+  },
+});
+
+export default App;
